3e7415e76d6f25f2545559144f4b84c2bf85369d
Brise <gabriel_ns@live.com>
2020-05-03 22:07:52 -0300

    fix: react fragment to hold material-ui ref

--- src/components/Buttons/BaseButton.tsx
+++ src/components/Buttons/BaseButton.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import Link from "next/link";
 import Button from "@material-ui/core/Button";
 
-const ButtonLink = ({href, hrefAs, className, children}) => (
-  <Link href={href} as={hrefAs} prefetch>
-    <a className={className}>{children}</a>
-  </Link>
+const ButtonLink = ({ href, hrefAs, className, children }) => (
+  <>
+    <Link href={href} as={hrefAs}>
+      <a className={className}>{children}</a>
+    </Link>
+  </>
 );
 
 // https://material-ui.com/demos/buttons/#third-party-routing-library
